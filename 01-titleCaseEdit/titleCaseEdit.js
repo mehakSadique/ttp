4cee07829aa0f0b8f672769e0e058360c4a9bfb1
mehakSadique <49571264+mehakSadique@users.noreply.github.com>
2020-05-27 09:49:48 -0400

    Update titleCaseEdit.js

--- 01-titleCaseEdit/titleCaseEdit.js
+++ 01-titleCaseEdit/titleCaseEdit.js
@@ -1,5 +1,14 @@
 function titleCaseEdit(title) {
   // Insert code here;
+  let titleCase = title.split(" ");
+
+ 
+  for (let i=0; i<titleCase.length; i++) {
+    titleCase[i] = titleCase[i].charAt(0).toUpperCase() + titleCase[i].substr(1);
+  }
+
+
+  return titleCase.join(' ');
 }
 
 // Do not edit this line;
