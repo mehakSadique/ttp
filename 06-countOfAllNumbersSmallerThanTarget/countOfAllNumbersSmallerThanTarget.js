baef6f633565c3b3f7467ee75aafd985da74aa36
mehakSadique <49571264+mehakSadique@users.noreply.github.com>
2020-06-01 10:34:26 -0400

    Update countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,13 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
   // Insert code here;
+   let count = 0;
+  for(var i = 0; i < nums.length; i++){
+    if(nums[i] < target){
+      count++;
+    }
+  }
+  //console.log(count);
+  return count;
 }
 
 // Do not edit this line;
